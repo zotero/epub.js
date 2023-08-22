e7b6b0cb8b3da04bdf2f12b4caa15eb424de5e90
Abe Jellinek <jellinek@berkeley.edu>
2023-08-22 09:40:31 -0400

    Optimize uuid()

--- src/utils/core.js
+++ src/utils/core.js
@@ -22,13 +22,7 @@ const _URL = typeof URL != "undefined" ? URL : (typeof window != "undefined" ? (
  * @memberof Core
  */
 export function uuid() {
-	var d = new Date().getTime();
-	var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-		var r = (d + Math.random()*16)%16 | 0;
-		d = Math.floor(d/16);
-		return (c=="x" ? r : (r&0x7|0x8)).toString(16);
-	});
-	return uuid;
+	return crypto.randomUUID();
 }
 
 /**
